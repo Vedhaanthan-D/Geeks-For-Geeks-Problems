86ec4695c3700330d939ff193cd4d604813005dd
Vedhaanthan-D <vedhaanthan2006@gmail.com>
2025-08-07 19:32:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/4 Sum - All Quadruples/4-sum-all-quadruples.java
@@ -0,0 +1,33 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> fourSum(int[] nums, int target) {
+        // code here
+        Arrays.sort(nums);
+        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long sum=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
+                    if(sum==target){
+                         ArrayList<Integer> temp = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        res.add(temp);
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                    }
+                    else if(sum <target) k++;
+                    else l--;
+                }
+            }
+        }
+        return res;
+    }
+}
